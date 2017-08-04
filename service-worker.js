ebd3bff06429bd752145ec3433a8b0d5893acfe1
Michael McGreevy <mcgreevy@chromium.org>
2017-08-04 15:27:29 +1000

    fetch from cache

--- service-worker.js
+++ service-worker.js
@@ -8,3 +8,18 @@ self.addEventListener('install', function(e) {
     })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
