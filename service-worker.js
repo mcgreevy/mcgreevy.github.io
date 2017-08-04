5673c82b1341bbdcbc93f4724da134fb4c32773f
Michael McGreevy <mcgreevy@chromium.org>
2017-08-04 14:47:03 +1000

    add some slashes

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ self.addEventListener('install', function(e) {
     caches.open('mycache').then(function(cache){
       return cache.addAll([
 	      '/',
-              "?utm_source=homescreen",
+              "/?utm_source=homescreen",
       ]);
     })
   );
